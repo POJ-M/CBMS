2c7f49a227995f15c1465db1f99f579b7485293a
POJ-M <itpresenceofjesus@gmail.com>
2026-03-05 14:36:02 +0530

    Delete backend directory

--- backend/routes/dashboard.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { protect, adminOnly } = require('../middleware/auth');
-const { getDashboardStats } = require('../controllers/dashboard.controller');
-
-router.use(protect, adminOnly);
-router.get('/', getDashboardStats);
-
-module.exports = router;
--- backend/utils/catchAsync.js
+++
@@ -1,9 +0,0 @@
-/**
- * Wraps async controller functions to eliminate try/catch boilerplate.
- * Passes any thrown error to Express next() for centralized handling.
- */
-const catchAsync = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next);
-};
-
-module.exports = catchAsync;
